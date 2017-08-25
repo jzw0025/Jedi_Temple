e8681618f85c5a24507d22f2bf05fe968909f56c
“Junchao” <jzw0025@auburn.edu>
2017-08-25 17:31:42 -0400

    homework 3

---
+++ HW3_FEM/CA2a/CMakeFiles/main2a.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/junchaowei/Desktop/testFEM2/CA2_template/CA2a/main2a.cc" "/Users/junchaowei/Desktop/testFEM2/CA2_template/CA2a/CMakeFiles/main2a.dir/main2a.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DEBUG"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/junchaowei/Desktop/Jedi_Temple/dealii/include"
+  "/Users/junchaowei/Desktop/Jedi_Temple/dealii/bundled/umfpack/UMFPACK/Include"
+  "/Users/junchaowei/Desktop/Jedi_Temple/dealii/bundled/umfpack/AMD/Include"
+  "/Users/junchaowei/Desktop/Jedi_Temple/dealii/bundled/boost-1.62.0/include"
+  "/Users/junchaowei/Desktop/Jedi_Temple/dealii/bundled/muparser_v2_2_4/include"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ VTK_PYTHON_Visualization.py
@@ -0,0 +1,35 @@
+from vtk import *
+# The source file
+file_name = "/Users/junchaowei/Desktop/Jedi_Temple/CA2_template/CA2a/solution.vtk"
+
+# Read the source file.
+reader = vtkUnstructuredGridReader()
+reader.SetFileName(file_name)
+reader.Update() # Needed because of GetScalarRange
+output = reader.GetOutput()
+scalar_range = output.GetScalarRange()
+
+# Create the mapper that corresponds the objects of the vtk file
+# into graphics elements
+mapper = vtkDataSetMapper()
+mapper.SetInputData(output)
+mapper.SetScalarRange(scalar_range)
+
+# Create the Actor
+actor = vtkActor()
+actor.SetMapper(mapper)
+
+# Create the Renderer
+renderer = vtkRenderer()
+renderer.AddActor(actor)
+renderer.SetBackground(1, 1, 1) # Set background to white
+
+# Create the RendererWindow
+renderer_window = vtkRenderWindow()
+renderer_window.AddRenderer(renderer)
+
+# Create the RendererWindowInteractor and display the vtk_file
+interactor = vtkRenderWindowInteractor()
+interactor.SetRenderWindow(renderer_window)
+interactor.Initialize()
+interactor.Start()
